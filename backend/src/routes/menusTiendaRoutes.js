c540301ed539e347ce0e2263b7e43827a70670d2
Marcelo <mabc@live.cl>
2021-07-11 11:12:38 -0400

    Se implementóó el menú lateral de la tienda (FrontOffice)

--- backend/src/routes/menusTiendaRoutes.js
+++ backend/src/routes/menusTiendaRoutes.js
@@ -2,7 +2,7 @@ const menusTiendaModel = require('../models/menusTienda');
 const checkToken = require('../shared/middlewares/mw_checkToken');
 
 module.exports = function(app, passport){
-    app.get('/menus_tienda', checkToken, (req, res) => {
+    app.get('/menus_tienda', (req, res) => {
         menusTiendaModel.mainMenu((err, data) => {
             res.json(err ? err : data);
         });
--- frontend/src/components/frontOffice/leftMenu/leftMenu.jsx
+++ frontend/src/components/frontOffice/leftMenu/leftMenu.jsx
@@ -1,16 +1,40 @@
-import React from 'react' 
+import React, { useEffect } from 'react' 
 import Drawer from 'react-modern-drawer'    //yarn add react-modern-drawer
+import { useSelector, useDispatch } from 'react-redux'
+import { getMainMenu } from '../../../actions/menusTienda'
 import 'react-modern-drawer/dist/index.css'
 import './style.css'
 
 export const LeftMenuComponent = (props) => {
     const { sowMenu, toggleMenu } = props
+    const menuMostradoState = useSelector(state => state.MenusTiendaReducer.displayedMenus)
+    const dispatch = useDispatch()
+
+
+    useEffect(()=>{
+        dispatch(getMainMenu())
+    },[dispatch])
+
+
+    const subMenu = (keyMenuPadre, sub_menu) => {
+        return sub_menu.map((mnu, key) => 
+            <li key={`${keyMenuPadre}_${key}`} className="li_menu_categoría">{mnu.nombre}</li>
+        )
+    }
+
 
     return (
         <>
             <Drawer open={sowMenu} onClose={toggleMenu} direction='left' className="left-menu">
                 <label className="close-left-menu" onClick={() => toggleMenu()}>X</label>
-                <div>Hello World</div>
+                <div className="title-left-menu">Categorías</div>
+                {
+                    menuMostradoState.map((mnu, key)=> {
+                        return  <ul key={key} className="ul_menu_categoria">{mnu.nombre}
+                                    {subMenu(key, mnu.sub_menu)}
+                                </ul>
+                    })
+                }
             </Drawer>
         </>
     )
