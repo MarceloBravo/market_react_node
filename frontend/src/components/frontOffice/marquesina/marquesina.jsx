f218badc83ab7b88badb7e3d3e43c54f52edf0c8
Marcelo <mabc@live.cl>
2021-07-17 21:49:52 -0400

    Se agregó la marquesina a la página home de la tienda (frontOffice)

---
+++ frontend/src/components/frontOffice/marquesina/marquesina.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react'
+import { Carousel } from 'react-bootstrap'
+import { useSelector, useDispatch } from 'react-redux'
+import { getData } from '../../../actions/imagenesMarquesina'
+import { defaultImagesTienda } from '../../../shared/constantes'
+import './style.css'
+
+
+export const Marquesina = () => {
+    const imagenesState = useSelector(state => state.ImagenesMarquesinaReducer.imagenes)
+    const dispatch = useDispatch()
+
+    useEffect(()=> {
+        dispatch(getData())
+    },[dispatch])
+    
+
+    return (
+        <Carousel fade variant="dark">
+            { imagenesState.map((i, key) => {
+                return <Carousel.Item key={key}>
+                        <img
+                        className="d-block w-100"
+                        src={defaultImagesTienda + i.src_imagen}
+                        alt={i.texto}
+                        />
+                        {(i.texto || i.link) && 
+                            <Carousel.Caption>
+                                <h5>{i.texto}</h5>
+                                <p>{i.link}</p>
+                            </Carousel.Caption>
+                                    }
+                    </Carousel.Item>
+                })
+            }
+        </Carousel>
+    )
+}
--- frontend/src/pages/frontOffice/home/home.jsx
+++ frontend/src/pages/frontOffice/home/home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { HeaderMarketComponent } from '../../../components/frontOffice/header/header'
 import { FooterComponent } from '../../../components/frontOffice/footer/footer'
+import { Marquesina } from '../../../components/frontOffice/marquesina/marquesina'
 
 export const HomeMarketComponent = () => {
 
     return (
         <>
             <HeaderMarketComponent/>
-            
+            <Marquesina />
             <FooterComponent/>
         </>
     )
