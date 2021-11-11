6bff11a006387e30c6bd566e1f3538cf728a4d44
Marcelo <mabc@live.cl>
2021-11-11 08:35:14 -0300

    Se separó el código del html en el listado de clientes del mantenedor de clientes close #3

--- frontend/src/pages/backOffice/clientes/grid/grid.jsx
+++ frontend/src/pages/backOffice/clientes/grid/grid.jsx
@@ -1,15 +1,9 @@
-import React, { useState, useEffect } from 'react'
-import { Header } from '../../../../components/backOffice/header'
-import { SpinnerComponent } from '../../../../components/shared/spinner'
-import { ModalDialog } from '../../../../components/backOffice/modalDialog'
-import { Menu } from '../../../../components/backOffice/menu'
-import { Alerta } from '../../../../components/shared/alerts'
-import { Grid } from '../../../../components/backOffice/grid'
-import { Paginacion } from '../../../../components/backOffice/paginacion'
+import { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { getPage, filter, deleteReg } from '../../../../actions/clientes'
 import { types as spinnerTypes } from '../../../../redux/Spinner/types'
 import { types as  modalTypes } from '../../../../redux/ModalDialog/types'
+import { ClientesGridContent } from './content'
 
 export const GridMntClientes = () => {
     const [ idDelete, setIdDelete ] = useState(null)
@@ -70,27 +64,13 @@ export const GridMntClientes = () => {
 
 
     return (
-        <div> 
-            <Menu activeKeyMenu="19"/>
-            <SpinnerComponent />
-            <ModalDialog response={response}/>
-            <div className={"main-section " + (togleMenu ? 'main-width' : 'main-normal')}>
-                <Header />
-                <div className="content-section">                    
-                    <Alerta />
-                    <Grid
-                        data={dataGrid}
-                        headers={['Nombre', 'Apellido 1', 'Apellido 2', 'Dirección', 'Ciudad', 'Fecha creación', 'Fecha actualización']}
-                        visibleFields={['nombres', 'apellido1', 'apellido2', 'direccion', 'ciudad', 'created_at', 'updated_at']}
-                        actionColumn={true}
-                        title={'Mantenedor de Clientes'}
-                        urlToForm={'clientes'}
-                        onClickDelete={e => eliminarRegistro(e)}
-                        onChangeFilter={e => fnFiltrar(e)}
-                    />
-                    <Paginacion data={dataGrid} goToPage={goToPage}/>
-                </div>
-            </div>
-        </div>
+        <ClientesGridContent 
+            response={response} 
+            togleMenu={togleMenu} 
+            dataGrid={dataGrid}  
+            eliminarRegistro={eliminarRegistro} 
+            fnFiltrar={fnFiltrar} 
+            goToPage={goToPage}
+        />
     )
 }
