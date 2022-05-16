3c7894fd3b09c29d2ecb2c61085a83b1a81afae0
Marcelo <mabc@live.cl>
2022-05-16 14:04:12 -0400

    Se corrigieron errores varios

--- backend2/src/app.js
+++ backend2/src/app.js
@@ -3,16 +3,8 @@ const express = require('express');
 var publicDir = require('path').join(__dirname,'/public');  //Configurando la carpeta public que contiene las imágenes
 
 const app = express();
-app.use(express.static(publicDir)); 
 app.use(cors());
-/*
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});*/
+app.use(express.static(publicDir)); 
 
 const morgan = require('morgan');   //
 const bodyParser = require('body-parser');  //Necesario para atender las peticiones post
